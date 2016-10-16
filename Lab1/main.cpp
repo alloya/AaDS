cab7e71b69f86b7e483f10f7665a829b971dff01
Yulia Lushinskaya <ylu@portia.ru>
2016-10-16 22:04:57 +0400

    alpha version

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,210 +1,191 @@
-#include <iostream>
+#include <vector>
+#include <sstream>
 #include <fstream>
-#include <string>
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
-#include <locale.h>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <array>
+#include <queue>
 
 using namespace std;
 
-int p = 0;
-int i = 0;
-int k = 0;
-int z = 0;
-char *imo;
-struct spy {
+struct SpyData
+{
 	char name[20];
-	int period;
-	int time;
-	spy *next;
-};
-struct spy kol[100];
-
-struct Node //описание узла списка
-{
-	char *name;
-	int period;
-	int time;
-	Node *next; //указатель на следующий элемент
+	int activeTime;
+	int allTime;
 };
 
-struct Queue //описание очереди
+void DeleteSpyFromQueue(vector<SpyData> &spyQueue, int spyNumber, int timeMoment)
 {
-	int size; //счетчик размера очереди
-	Node *first; //указатель на начало очереди
-	Node *last; //указатель на конец очереди
-};
-
-void Creation(Queue *Q) //создание очереди
-{
-	Q->first = new Node;
-	Q->first->next = NULL;
-	Q->last = Q->first;
-	Q->size = 0;
+	vector<SpyData>::iterator number = spyQueue.begin();
+	number += spyNumber;
+	cout << "Moment " << timeMoment << " : " << spyQueue[spyNumber].name << " left queue." << endl;
+	//cout << "vector size = " << spyQueue.size() << endl ;
+	spyQueue.erase(number);
+	//cout << "vector size = " << spyQueue.size() << endl ;
 }
 
-void Print(Queue *Q, char kolname, int kolperiod, int kolvrem9)
+//void isAnySpyHasToGo(vector<SpyData> &spyQueue, int time)
+//{
+//	for (int i = 0; i < spyQueue.size(); i++)
+//	{
+//		if (time >= spyQueue[i].allTime)
+//		{
+//			DeleteSpyFromQueue(spyQueue, i, time);
+//		}
+//	}
+//}
+
+void isAnySpyHasToGoRightNow(vector<SpyData> &spyQueue, int time)
 {
-	Node *tmp = Q->first;
-	for (i = 0; i < k; i++)
+	for (int i = 0; i < spyQueue.size(); i++)
 	{
-		printf("%s %d %d\n", tmp->next->name, tmp->next->period, tmp->next->time);
-		tmp = tmp->next;
+		if (time >= spyQueue[i].allTime)
+		{
+			DeleteSpyFromQueue(spyQueue, i, time);
+			//cout << "Deleted elem" << i << " in isAnySpyHasToGoRightNow" << endl;
+		}
 	}
 }
-bool Full(Queue *Q) //проверка очереди на пустоту
-{
-	if (Q->first == Q->last) return true;
-	else return false;
-}
-void Add(Queue *Q, char kolname, int kolperiod, int kolvrem9) //добавление элемента
-{
-	int value;
-	int voz;
-	char *im9;
-	im9 = kol[i].name;
-	value = kol[i].period;
-	voz = kol[i].time;
-	Q->last->next = new Node;
-	Q->last = Q->last->next;
-	Q->last->name = im9; //добавление элемента в конец
-	Q->last->time = voz;
-	Q->last->period = value;
-	Q->last->next = NULL; //обнуление указател€ на следующий элемент
-	Q->size++;
-	imo = Q->last->name = im9;
-}
-void Delete(Queue *Q) //удаление элемента
-{
-	Node *tmp = Q->first;
-	Q->first = Q->first->next; //смещение указател€
-	Q->size--;
-	delete tmp;
-	printf("\nЁлемент удален\n\n");
-};
 
-int Size(Queue *Q) //размер очереди
+void MoveQueue(vector<SpyData> &spyQueue)
 {
-	return Q->size;
+	//cout << spyQueue[0].name << endl;
+	spyQueue.push_back(spyQueue[0]);
+	//cout << "first elem pushed back" << endl;
+	spyQueue.erase(spyQueue.begin());
+	//cout << spyQueue[0].name << endl;
 }
 
-int Proverka(Queue *Q)
+void CalculateQueue1(vector<SpyData> &spyQueue)
 {
-	FILE *output;
-	int s = 0;
-	Node *tmp = Q->first;
-	for (i = 0; i < k; i++)
+	int timeMoment = 0;
+	cout << "The watch has begun." << endl;
+	while (spyQueue.size() != 1)
 	{
-		//file = fopen("output.txt", "a");
 		
-		z += tmp->next->period;
-		if (z >= tmp->next->time) {
-			fprintf(output, "¬рем€ %d\n", tmp->next->time);
-			fprintf(output, "%s-покинул очередь\n", tmp->next->name);
-			z = z - tmp->next->period;
-			tmp = tmp->next;
-			return 1;
-		}
-		else {
-			fprintf(output, "¬рем€ %d\n", z - tmp->next->period);
-			fprintf(output, "%s-начало наблюдени€\n", tmp->next->name);
-			if (z - tmp->next->period > 0)
-				fprintf(output, "%s-конец наблюдени€\n", imo);
-			tmp = tmp->next;
-			return 0;
+		for (int i = 0; i < 3; i++)
+		{
+			isAnySpyHasToGoRightNow(spyQueue, timeMoment);
+			cout << "Moment " << timeMoment << " : " << spyQueue[i].name << " started his watch." << endl;
+			timeMoment += spyQueue[i].activeTime;
+			MoveQueue(spyQueue);
+			cout << "next iteration" << endl;
 		}
 	}
-
-}
-void Add1(Queue *Q, char kolname, int kolperiod, int kolvrem9) //добавление элемента
-{
-	Node *tmp = Q->first;
-	int value;
-	int voz;
-	char *im9;
-	im9 = tmp->next->name;
-	value = tmp->next->period;
-	voz = tmp->next->time;
-	Q->last->next = new Node;
-	Q->last = Q->last->next;
-	Q->last->name = im9; //добавление элемента в конец
-	Q->last->time = voz;
-	Q->last->period = value;
-	Q->last->next = NULL; //обнуление указател€ на следующий элемент
-	Q->size++;
-	imo = Q->last->name = im9;
+	cout << "Moment " << spyQueue[0].allTime << " : " << spyQueue[0].name << " ended his watch." << endl;
+	spyQueue.clear();
 }
 
-int main(int argc, char * argv[])
+bool IsHeWillStayForFullWatch(vector<SpyData> &vector, int currSpy, int currtime)
 {
-	setlocale(LC_ALL, "Russian");
-	
-	if (argc != 3)
+	if (vector[currSpy].allTime < (currtime + vector[currSpy].activeTime))
 	{
-		cout << "Invalid arguments count\n"
-			<< "Usage: copyfile.exe <input file> <output file>\n";
-		return 1;
+		//int newTime = vector[currSpy].allTime;
+		//cout << "CheckForFullWatch = true" << endl;;
+		//DeleteSpyFromQueue(vector, currSpy, vector[currSpy].allTime);
+		//return vector[currSpy].allTime;
+		return true;
 	}
+	return false;
+}
 
-	ifstream input(argv[1]);
-	if (!input.is_open())
+void PrintVector(vector<SpyData> vector)
+{
+	//cout << "vector order:  ";
+	for (int k = 0; k < vector.size(); ++k)
 	{
-		cout << "Failed to open " << argv[1] << " for reading\n";
-		return 1;
+		cout.width(15);
+		cout << vector[k].name << "\t\tActive time: " << vector[k].activeTime << "\tAll time: " << vector[k].allTime << endl;
 	}
+	//cout << endl;
+}
 
-	ofstream output(argv[2]);
-	if (!output.is_open())
+//void CalculateQueue2(vector<SpyData> &vector)
+//{
+//	int currTime = 0;
+//	while(vector.size())
+//	{
+//		//int i = 0;
+//		for (int i = 0; i < vector.size(); i++)
+//		{
+//			cout << "Moment " << currTime << " : " << vector[i].name << " started his watch." << endl;
+//			if (!isAnySpyHasToGoRightNow)
+//			{
+//				DeleteSpyFromQueue(vector, i, currTime);
+//			}
+//			cout << "before check for full watch" << endl;
+//			IsHeWillStayForFullWatch(vector, i, currTime);
+//			cout << "before move queue" << endl;
+//			currTime += vector[i].activeTime;
+//			MoveQueue(vector);
+//		}
+//		//i++;
+//	}
+//}
+
+void CalculateQueue(vector<SpyData> &vector)
+{
+	int currTime = 0;
+
+	while (vector.size() != 1)
 	{
-		cout << "Failed to open " << argv[2] << " for writing\n";
-		return 1;
+		for (int i = 0; i < vector.size(); i++)
+		{
+			cout << "iteration # " << i << endl;
+			cout << "curr time = " << currTime << endl;
+			//int activeTimeOfCurrSpy = vector[i].activeTime;
+			cout << "Moment " << currTime << " : " << vector.front().name << " started his watch." << endl;
+			isAnySpyHasToGoRightNow(vector, currTime);
+			if (IsHeWillStayForFullWatch(vector, i, currTime))
+			{
+				currTime = vector[i].allTime;
+			}
+			else
+			{
+				currTime += vector[i].activeTime;
+			}
+			//currTime += activeTimeOfCurrSpy;
+			
+			MoveQueue(vector);
+			cout << endl;
+		}
 	}
+	cout << "Moment " << vector.front().allTime << " : " << vector.front().name << " left queue." << endl;
+	return;
+}
 
-	
-	FILE *file;
-	Queue Q;
-	Creation(&Q);
-	int j = 0;
-	int y = 0;
-	int z = 0;
-	char *kolname;
-	int kolperiod;
-	int kolvrem9;
-	
-	file = fopen("test.txt", "r");
-	while (fscanf(file, "%s%d%d", kol[i].name, &(kol[i].period), &(kol[i].time)) != EOF) {
+int main()
+{
+	SpyData spy[100];
+	vector<SpyData> spyQueue;
+	FILE *data;
+	int i = 0;
+	if ((data = fopen("spy.txt", "r")) == NULL)
+	{
+		printf("ошибка при прочтении");
+		exit(1);
+	}
+	data = fopen("spy.txt", "r");
+	while (fscanf(data, "%s%d%d", spy[i].name, &(spy[i].activeTime), &(spy[i].allTime)) != EOF) 
+	{
+		spyQueue.push_back(spy[i]);
 		i++;
 	}
-	fclose(file);
-	p = i;
-	for (i = 0; i < p; i++) {
-		kolname = kol[i].name;
-		kolperiod = kol[i].period;
-		kolvrem9 = kol[i].time;
-		Add(&Q, *kolname, kolperiod, kolvrem9);
-	}
-	k = Size(&Q);
-	if (Full(&Q)) fprintf(file, "ќчередь пуста");
-	else Print(&Q, *kolname, kolperiod, kolvrem9);
-	system("pause");
-	while (k != 0) {
-		if (Proverka(&Q) == 0) {
-			Add1(&Q, *kolname, kolperiod, kolvrem9);
-			Delete(&Q);
-		}
-		else
-			Delete(&Q);
-		k = Size(&Q);
-	}
+	
+	PrintVector(spyQueue);
 
-	if (!output.flush()) 
-	{
-		cout << "Failed to save data on disk\n";
-		return 1;
-	}
-
+	//MoveQueue(spyQueue);
+	//PrintVector(spyQueue);
+	//isAnySpyHasToGo(spyQueue, 5);
+	//DeleteSpyFromQueue(spyQueue, 1, spyQueue[1].allTime);
+	//PrintVector(spyQueue);
+	//CheckForFullWatch(spyQueue, 1, 4);
+	CalculateQueue(spyQueue);
+	//PrintVector(spyQueue);
 	return 0;
 }
+
 
 
